4bad993cf942152932e2a40ef6717e4feb61315d
Mario <mario.fleischmann99@t-online.de>
2022-03-24 19:54:16 +0100

    Add gray code generation

--- c/mixed_radix.c
+++ c/mixed_radix.c
@@ -8,13 +8,42 @@
  * returns 1 if overflow has happened
  * otherwise 0
  */
-int increment(unsigned char *digits, unsigned char *bases, int n_digits) {
-  for (int i = n_digits - 1; i >= 0; i--) {
-    if (digits[i] >= bases[i]-1) {
+int increment(unsigned int *digits, unsigned int *radix, int n_digits) {
+  for (int i = n_digits-1; i >= 0; i--) {
+    if (digits[i] >= radix[i]-1) {
       digits[i] = 0;
     } else {
       digits[i]++;
       return 0;
+    }
+  }
+
+  return 1;
+}
+
+/*
+ * increments a number by one
+ * according to gray code
+ *
+ * returns 1 if overflow has happened
+ * otherwise 0
+ */
+int increment_gray(unsigned int *digits, unsigned int *radix, char *is_decreasing, int n_digits) {
+  for (int i = n_digits-1; i >= 0; i--) {
+    if (is_decreasing[i]) {
+      if (digits[i] <= 0) {
+        is_decreasing[i] = 0;
+      } else {
+        digits[i]--;
+        return 0;
+      }
+    } else {
+      if (digits[i] >= radix[i]-1) {
+        is_decreasing[i] = 1;
+      } else {
+        digits[i]++;
+        return 0;
+      }
     }
   }
 
@@ -27,10 +56,10 @@ int increment(unsigned char *digits, unsigned char *bases, int n_digits) {
  * returns 1 if underflow has happened
  * otherwise 0
  */
-int decrement(unsigned char *digits, unsigned char *bases, int n_digits) {
+int decrement(unsigned int *digits, unsigned int *radix, int n_digits) {
   for (int i = n_digits - 1; i >= 0; i--) {
     if (digits[i] <= 0) {
-      digits[i] = bases[i] - 1;
+      digits[i] = radix[i] - 1;
     } else {
       digits[i]--;
       return 0;
@@ -46,7 +75,7 @@ int decrement(unsigned char *digits, unsigned char *bases, int n_digits) {
  * returns 1 if number is zero
  * otherwise 0
  */
-int is_zero(unsigned char *digits, int n_digits) {
+int is_zero(unsigned int *digits, int n_digits) {
   int z = 1;
   for (int i = 0; i < n_digits; i++)
     z &= (digits[i] == 0);
@@ -61,10 +90,10 @@ int is_zero(unsigned char *digits, int n_digits) {
  * returns 1 if number is zero
  * otherwise 0
  */
-int is_max(unsigned char *digits, unsigned char *bases, int n_digits) {
+int is_max(unsigned int *digits, unsigned int *radix, int n_digits) {
   int m = 1;
   for (int i = 0; i < n_digits; i++)
-    m &= (digits[i] == bases[i]-1);
+    m &= (digits[i] == radix[i]-1);
 
   return m;
 }
@@ -72,15 +101,18 @@ int is_max(unsigned char *digits, unsigned char *bases, int n_digits) {
 /*
  * Prints a number.
  */
-void print_number(unsigned char *digits, int n_digits) { 
+void print_number(unsigned int *digits, int n_digits) { 
   for (int i = 0; i < n_digits; i++)
     printf("%d", digits[i]);
   printf("\n");
 }
 
 int main(void) {
-  unsigned char digits[N_MAX] = {0}; 
-  unsigned char bases[N_MAX];
+  unsigned int digits[N_MAX] = {0}; 
+  unsigned int radix[N_MAX];
+#ifdef USE_GRAY
+  char is_decreasing[N_MAX] = {0};
+#endif
 
   int n_digits = 0;
   int base;
@@ -89,13 +121,23 @@ int main(void) {
       printf("Base value <= 0 recognized. Program terminates.\n");
       return -1;
     }
-    bases[n_digits++] = base; 
+    radix[n_digits++] = base; 
   }
 
   unsigned long n = 0;
-  while (increment(digits, bases, n_digits) < 1) {
+  do {
     n++;
+
+#ifdef VERBOSE
+    print_number(digits, n_digits);
+#endif
+
   }
+#ifdef USE_GRAY
+  while (!increment_gray(digits, radix, is_decreasing, n_digits));
+#else
+  while (!increment(digits, radix, n_digits));
+#endif
 
   printf("Limit reached. Amount of increments: %ld\n", n);
   return 0;
